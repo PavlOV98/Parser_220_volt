5032835e4e4c6152a224408bcf99f4bc12e9fe59
PavlOV98 <oleg101-101@mail.ru>
2025-10-17 19:42:54 +0300

    out_file add

--- file_analys.py
+++ file_analys.py
@@ -1,11 +1,19 @@
 import os
 from bs4 import BeautifulSoup
+import json
 
 from constants import save_dir
 
-# Получаем список всех файлов и папок
-all_items = os.listdir(save_dir)
+def add_if_exist(key, data, dict):
+    if key in dict:
+        data[key] = dict[key]
+    else:
+        data[key] = None
 
+
+    # Получаем список всех файлов и папок
+all_items = os.listdir(save_dir)
+d = {'items':[]}
 print(all_items)
 for file in all_items:
     with open(save_dir+file, 'r', encoding='windows-1251') as f:
@@ -21,13 +29,32 @@ for file in all_items:
         price = price[0].text.strip()
 
         print(name)
-        print(price)
-        arr = []
+        #print(price)
+        price = price[:price.index('р.')]
+        price = int(price.replace(' ', ''))
+        item = {'name':name, 'price':price}
+
+        arr = {}
         for line in table_content:
             data = line.find_all('td')
             data[0] = data[0].text.strip()
             data[1] = data[1].text.strip()
             #print(data[0], data[1])
-            arr.append([data[0], data[1]])
-        print(arr)
+            arr[data[0]] = data[1]
+        #print(arr)
         #print(table.text)
+        keys = [
+            'Вес', 'Напряжение аккумулятора (В)',
+            'Макс. крутящий момент', 'Наличие удара',
+            'Страна происхождения', 'Тип аккумулятора',
+            'Аккумуляторов в комплекте', 'Количество скоростей',
+            'Тип патрона', 'Емкость аккумулятора (Ач)'
+        ]
+        for key in keys:
+            add_if_exist(key, item, arr)
+
+
+        d['items'].append(item.copy())
+
+with open('out_file.json', 'w') as file:
+    json.dump(d, file, indent=4)
