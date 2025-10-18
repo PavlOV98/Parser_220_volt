dee60eeb40fb5eeac25e6a1d6888c179f7c9d51f
PavlOV98 <oleg101-101@mail.ru>
2025-10-18 11:04:20 +0300

    Update file_analys.py

--- file_analys.py
+++ file_analys.py
@@ -11,29 +11,39 @@ def add_if_exist(key, data, dict):
         data[key] = None
 
 
-    # Получаем список всех файлов и папок
+# Получаем список всех файлов и папок
 all_items = os.listdir(save_dir)
-d = {'items':[]}
 print(all_items)
+
+d = {'items':[]}
 for file in all_items:
+    # Открываем html файл для чтения, важно учесть кодировку
     with open(save_dir+file, 'r', encoding='windows-1251') as f:
+        # Создаём объект BeautifulSoup
         html_content = f.read()
         soup = BeautifulSoup(html_content, 'html.parser')
 
+        # Достаём имя по классу
         name = soup.select('.card-main-title')
         name = name[0].text.strip()
-        table = soup.find('table')
-        table_content = table.find_all('tr')
-        #print(table_content[0].text)
+
+        # Достаём цену по классу, переводим в число
         price = soup.select('.card-lg-price')
         price = price[0].text.strip()
+        price = price[:price.index('р.')]
+        price = int(price.replace(' ', ''))
+
+        # Достаём таблицу по тегу
+        table = soup.find('table')
+        # Из таблицы берём все строки
+        table_content = table.find_all('tr')
+        #print(table_content[0].text)
 
         print(name)
         #print(price)
-        price = price[:price.index('р.')]
-        price = int(price.replace(' ', ''))
         item = {'name':name, 'price':price}
 
+        # Достаём из строк таблицы данные
         arr = {}
         for line in table_content:
             data = line.find_all('td')
@@ -43,6 +53,8 @@ for file in all_items:
             arr[data[0]] = data[1]
         #print(arr)
         #print(table.text)
+
+        # Выбираем нужные параметры
         keys = [
             'Вес', 'Напряжение аккумулятора (В)',
             'Макс. крутящий момент', 'Наличие удара',
@@ -54,7 +66,9 @@ for file in all_items:
             add_if_exist(key, item, arr)
 
         #print(item)
+        # Добавляем данные в список
         d['items'].append(item.copy())
 
+# Сохраняем данные в JSON файл
 with open('out_file.json', 'w') as file:
     json.dump(d, file, indent=4)
